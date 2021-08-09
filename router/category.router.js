340721df03fc409a73d62a6767600d363f2a3690
hangnguyenn <thuyhanghcmus@gmail.com>
2021-08-09 19:07:39 +0700

    Stock and Category CRUD
    - Get all, get id, update, delete id, create

--- router/category.router.js
+++ router/category.router.js
@@ -3,8 +3,9 @@ const controller = require('../controller/category.controller');
 const authMiddleware = require("../middleware/auth")
 const router = express.Router();
 
-router.get('/', controller.getAll);
-router.get('/:id', controller.getById);
-router.post('/', controller.add);
-
+router.get('/', authMiddleware.isAuth, controller.getAll);
+router.get('/:id', authMiddleware.isAuth, controller.getById);
+router.post('/', authMiddleware.isAuth, controller.add);
+router.get('/delete/:id', authMiddleware.isAuth, controller.delete);
+router.post('/update', authMiddleware.isAuth, controller.update);
 module.exports = router;
--- router/stock.router.js
+++ router/stock.router.js
@@ -3,6 +3,10 @@ const controller = require('../controller/stock.controller');
 const authMiddleware = require("../middleware/auth")
 const router = express.Router();
 
-router.get('/',authMiddleware.isAuth,controller.getAll);
+router.get('/', authMiddleware.isAuth, controller.getAll);
+router.get('/:id', authMiddleware.isAuth, controller.getById);
+router.post('/', authMiddleware.isAuth, controller.add);
+router.get('/delete/:id', authMiddleware.isAuth, controller.delete);
+router.post('/update', authMiddleware.isAuth, controller.update);
 
 module.exports = router;
