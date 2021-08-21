f781f7a98b810eef589f94f38ce807a931145900
hangnguyenn <thuyhanghcmus@gmail.com>
2021-08-21 23:08:01 +0700

    Product: Get top month and get suggestion

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -10,7 +10,8 @@ const asyncHandler = require('express-async-handler');
 module.exports.getAll = asyncHandler(async function (req, res, next) {
     let result = await Product.getAll();
     var prodList = []
-    var index = 0
+    var index = 0;
+
     while (index < result.length) {
         let prodObj = {
             prod_id: result[index].id,
@@ -55,42 +56,101 @@ module.exports.getAll = asyncHandler(async function (req, res, next) {
 
 module.exports.getTopMonth = asyncHandler(async function (req, res, next) {
     let result = await Product.getTopMonth();
-    if (!result) return res.status(400).send({ message: 'Get failed' });
-    let topOfMonth = [result[0], result[1], result[3], result[10], result[4]]
-    return res.status(200).send(topOfMonth);
+    
+    var listProduct = [];
+    if(result.rows.length > 0) {
+        for(var i= 0; i< result.rows.length; i++) {
+            const id_cate = result.rows[i].id;
+            const images = [];
+            images.push(result.rows[i].data);
+            for(var j= i+ 1; j< result.rows.length; j++) {
+                if(id_cate === result.rows[j].id) {
+                    images.push(result.rows[j].data);
+                }
+            }
+            //check exist in listProduct
+            var existCategory= false;
+            if(listProduct) {
+                listProduct.forEach(element => {
+                    if(id_cate === element.prod_id) {
+                        existCategory = true;
+                    }
+                });
+            }
+
+            if(!existCategory) {
+                listProduct.push({
+                    prod_id: id_cate,
+                    prod_name: result.rows[i].name,
+                    prod_category_id: result.rows[i].cate_id,
+                    prod_amount: result.rows[i].amount,
+                    prod_created_date: result.rows[i].created_date,
+                    prod_updated_date: result.rows[i].updated_date,
+                    prod_price: result.rows[i].price,
+                    image: images
+                })
+                
+            }
+        }
+    }
+
+    if (result) {
+        return res.status(200).json({
+            listProduct: listProduct,
+            statusCode: 0
+        })
+    }
+    else {
+        return res.status(500).json({
+            listProduct: [],
+            statusCode: 1
+        })
+    }
 });
 
 module.exports.getSuggestion = asyncHandler(async function (req, res, next) {
     let result = await Product.getSuggestion();
-    if (!result) return res.status(400).send({ message: 'Get failed' });
-    var prodList = []
-    var index = 0
-    while (index < result.length) {
-        let prodObj = {
-            prod_id: result[index].id,
-            prod_name: result[index].prod_name,
-            prod_category_id: result[index].cate_id,
-            vote: result[index].vote,
-            prod_amount: result[index].amount,
-            prod_created_date: result[index].created_date,
-            prod_updated_date: result[index].updated_date,
-            prod_price: result[index].price
-        }
-        let imageLink = []
-        for (let i = index; i < result.length; i++) {
-            index = i + 1
-            imageLink.push(result[i].data)
+    
+    var listProduct = [];
+    if(result.rows.length > 0) {
+        for(var i= 0; i< result.rows.length; i++) {
+            const id_cate = result.rows[i].id;
+            const images = [];
+            images.push(result.rows[i].data);
+            for(var j= i+ 1; j< result.rows.length; j++) {
+                if(id_cate === result.rows[j].id) {
+                    images.push(result.rows[j].data);
+                }
+            }
+            //check exist in listProduct
+            var existCategory= false;
+            if(listProduct) {
+                listProduct.forEach(element => {
+                    if(id_cate === element.prod_id) {
+                        existCategory = true;
+                    }
+                });
+            }
 
-            if ((i >= result.length - 1) || (i != 0 && result[index].id != result[index - 1].id)) {
-                break;
+            if(!existCategory) {
+                listProduct.push({
+                    prod_id: id_cate,
+                    prod_name: result.rows[i].name,
+                    prod_category_id: result.rows[i].cate_id,
+                    prod_amount: result.rows[i].amount,
+                    prod_created_date: result.rows[i].created_date,
+                    prod_updated_date: result.rows[i].updated_date,
+                    prod_price: result.rows[i].price,
+                    image: images
+                })
+                
             }
         }
-        prodObj['images'] = imageLink
-        prodList.push(prodObj)
     }
+
     if (result) {
         return res.status(200).json({
-            listProduct: prodList,
+            listProduct: listProduct,
             statusCode: 0
         })
     }
--- model/product.model.js
+++ model/product.model.js
@@ -8,20 +8,33 @@ async function getAll() {
 }
 
 async function getTopMonth() {
-    const data = await knex('products')
-        .join('product_images', 'products.id', '=', 'product_images.prod_id')
-        .join('comments', 'products.id', '=', 'comments.prod_id')
-        .orderBy('comments.vote', 'desc')
-        .limit(10);
+    const data = await knex.raw(`
+        select d.id, d.name, d.cate_id, d.price, e."data", d.amount, d.avg_vote 
+        from (
+            select a.id,  a."name", a.cate_id, a.price, a.amount, avg(b.vote) avg_vote
+            from products a, COMMENTS b
+            where a.id= b.prod_id
+            AND EXTRACT(MONTH FROM b.create_date) = EXTRACT(MONTH FROM CURRENT_TIMESTAMP)
+            group by a.id,  a."name", a.cate_id, a.price, a.amount) d, 
+            product_images e
+        where d.id= e.prod_id
+        order by d.avg_vote DESC
+        limit(10)` );
     return data;
 }
 
 async function getSuggestion() {
-    const data = await knex('products')
-        .join('product_images', 'products.id', '=', 'product_images.prod_id')
-        .join('comments', 'products.id', '=', 'comments.prod_id')
-        .orderBy('comments.vote', 'desc')
-        .limit(10);
+    const data = await knex.raw(`
+    select d.id, d.name, d.cate_id, d.price, e."data", d.amount, d.avg_vote 
+    from (
+        select a.id,  a."name", a.cate_id, a.price, a.amount, avg(b.vote) avg_vote
+        from products a, COMMENTS b
+        where a.id= b.prod_id
+        group by a.id,  a."name", a.cate_id, a.price, a.amount) d, 
+        product_images e
+    where d.id= e.prod_id
+    order by d.avg_vote DESC
+    limit(10)`);
     return data;
 }
 
