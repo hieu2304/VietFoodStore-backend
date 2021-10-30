95827cff3aebe72bc8ea092ee394b3485095900a
Hang Nguyen <thuyhanghcmus@gmail.com>
2021-10-31 01:02:11 +0700

    Fix add product into card

--- router/product.router.js
+++ router/product.router.js
@@ -3,7 +3,7 @@ const controller = require('../controller/product.controller');
 const router = express.Router();
 
 router.post('/list',controller.getAll);
-router.get('/detail/:id?',controller.getDetail);
+router.get('/details/:id?',controller.getDetail);
 router.post('/list-best-sale',controller.getTopMonth);
 router.post('/list-suggestion',controller.getSuggestion);
 router.post('/list-by-cat',controller.getListByCart)
